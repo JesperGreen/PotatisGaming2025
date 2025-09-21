e8acabc6f9dece7e6081471563370e8a2210f2c7
JesperGreen <jesper_gs_@hotmail.com>
2025-09-21 19:32:41 +0200

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -17,10 +17,31 @@ namespace PotatisGaming2025
         static int playerDamage;
         static int playerGold;
 
-        static string[] enemyNames = { };
+        static string[] enemyNames = { "Potato-Rat", "Potato-Goblin", "Potato-Skeleton", "Potato-Bandit" };
         public static void StartGame()
         {
+            Console.WriteLine("=== Potatis-Äventyr! ===");
+            Console.Write("Vad heter du?: ");
+            playerName = Console.ReadLine();
 
+            Console.WriteLine("Välj din klass:");
+            Console.WriteLine("1. Potatis-Warrior");
+            Console.WriteLine("2. Potatis-Mage");
+            Console.Write("Ditt val: ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+
+            }
+            else if (input == "2") 
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong choice, noob.");
+            }
         }
 
         public static void GameLoop()
