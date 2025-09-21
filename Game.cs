bf4774b3564c70fda60c31268b7c91a6c4921d66
JesperGreen <jesper_gs_@hotmail.com>
2025-09-21 19:10:52 +0200

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace PotatisGaming2025
+{
+    internal class Game
+    {
+
+        static string playerName;
+        static string playerClass;
+        static int playerHP;
+        static int playerMaxHP;
+        static int playerMana;
+        static int playerDamage;
+        static int playerGold;
+
+        static string[] enemyNames = { };
+        public static void StartGame()
+        {
+
+        }
+
+        public static void GameLoop()
+        {
+
+        }
+    }
+}
