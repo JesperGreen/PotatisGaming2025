86fc770886b7b6208c4600f66bcec53170f5b09d
JesperGreen <jesper_gs_@hotmail.com>
2025-09-21 19:10:24 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
+namespace PotatisGaming2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Potatis Gaming 2025, wow!");
+        }
+    }
+}
 
